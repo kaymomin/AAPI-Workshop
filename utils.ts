e0520a021de57f5a1439764a8f93a2cb8d7bf795
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2023-02-01 16:59:09 +0500

    updated

---
+++ utils.ts
@@ -0,0 +1,23 @@
+import AnkrscanProvider from '@ankr.com/ankr.js';
+
+// Create an instance of the AnkrscanProvider 
+const provider = new AnkrscanProvider('');
+
+// Export a function that returns token holders information
+export const getTokenHolders = async (contractAddress: string) => {
+  // Call the getTokenHolders method
+  //pass the contract address and (optional) blockchain type
+  const { holders, holdersCount } = await provider.getTokenHolders({
+    contractAddress,
+    blockchain: 'eth',
+  });
+  
+  // Log the holders to the console
+  console.log({ holders });
+  
+  // Return the holders and holdersCount
+  return {
+    holders,
+    holdersCount,
+  };
+};
